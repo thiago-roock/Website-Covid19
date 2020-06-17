8f22970a664f22a262f4e7f5abc083040bca0e0b
Thiago de Melo Lima <56129760+RDPodcasting@users.noreply.github.com>
2020-06-17 00:11:25 -0300

    travis-ci (#1)
    Co-authored-by: Thiago de Melo Lima <Thiago_badboy300@hotmail.com>

---
+++ Covid19.Web/Models/dataCovid19.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RdPodcastingWeb.Model
+{
+    public class DataCovid19
+    {
+        public string FIPS { get; set; }
+        public string Admin2 { get; set; }
+        public string Province_State { get; set; }
+        public string Last_Update { get; set; }
+        public string Lat { get; set; }
+        public string Long_ { get; set; }
+        public string Deaths { get; set; }
+        public string Recovered { get; set; }
+        public string Active { get; set; }
+        public string Combined_Key { get; set; }
+        public string Confirmed { get; set; }
+        public string Country_Region { get; set; }
+    }
+
+}
